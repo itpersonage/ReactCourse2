6ec7159dc756d35272711f26bfc3314a022f73d0
Andrey <it.personage@gmail.com>
2023-04-05 21:06:46 -0600

    lesson 2 react 75%

--- src/ProductCard/index.tsx
+++ src/ProductCard/index.tsx
@@ -7,23 +7,48 @@ interface TProps {
 
 export const ProductCard = ({ productName }: TProps) => {
   const [count, setCount] = useState(0);
+  const [limCount, setLimCount] = useState(0);
 
   const addCount = () => {
-    setCount(count + 1);
+    if (count !== limCount && count < limCount) {
+      setCount(count + 1);
+    }
   };
 
   const deleteCount = () => {
-    setCount(count - 1);
+    if (count !== 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const addLimCount = () => {
+    setLimCount(limCount + 1);
+  };
+
+  const delLimCount = () => {
+    if (limCount !== 0) {
+      setLimCount(limCount - 1);
+    }
+    if (count === limCount && count !== 0) {
+      setCount(count - 1)
+    }
   };
 
   return (
-    <div className="card_wrapper">
-      <span>{productName}</span>
-      <div className="button_section">
-        <button onClick={deleteCount}>delete</button>
-        <span>{count}</span>
-        <button onClick={addCount}>add</button>
+    <>
+      <div className="card_wrapper">
+        <span>{productName}</span>
+        <div className="button_section">
+          <button onClick={addCount}>add</button>
+          <span>{count}</span>
+          <button onClick={deleteCount}>delete</button>
+        </div>
       </div>
-    </div>
+      <div className="limit_count">
+          <button onClick={addLimCount}>add_max</button>
+          <span>{`  __  ${limCount}  __  `}</span>
+          <button onClick={delLimCount}>delete_max</button>
+        </div>
+    </>
   );
 };
