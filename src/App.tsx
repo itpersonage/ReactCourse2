1f6460aaf9d4cd7f42cf1fc878c195b1057d0cf9
Andrey <it.personage@gmail.com>
2023-04-18 19:43:08 -0600

    lesson 3 react 75%

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './App.module.scss';
+import { Route, Routes } from 'react-router-dom';
+import { MainPage } from './Containers/MainPage';
 import { PostsPage } from './Containers/PostsPage';
 
 function App() {
   return (
     <div className={styles.wrapper}>
-      <PostsPage />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/posts" element={<PostsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/Containers/MainPage/index.tsx
+++ src/Containers/MainPage/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import styles from './MainPage.module.scss';
 import { Header } from '../../Components/Commons/Header';
 import { Footer } from '../../Components/Commons/Footer';
 import { MainInfo } from '../../Components/Main/MainInfo';
 
 export const MainPage = () => {
   return (
-    <div>
+    <div className={styles.mainPageWrapper}>
       <Header />
-      <span>MainPage</span>
-      <MainInfo />
+      <div>
+        <span>MainPage</span>
+        <MainInfo />
+      </div>
       <Footer />
     </div>
   );
